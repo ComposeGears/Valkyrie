300655b90184cbddc9e12a9b332830ace902d59d
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-06-28 17:20:28 +0300

    create CodeBlockAnnotatedString

---
+++ plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/iconpack/creation/util/IconPackTooltipContent.kt
@@ -0,0 +1,46 @@
+package io.github.composegears.valkyrie.ui.screen.mode.iconpack.creation.util
+
+import androidx.compose.ui.text.AnnotatedString
+import io.github.composegears.valkyrie.ui.util.codeBlockAnnotatedString
+
+fun buildPackPackageHint(packageName: String, iconPackName: String): AnnotatedString {
+    val packagePlaceholder = packageName.ifEmpty { "your.package" }
+    val iconPackPlaceholder = iconPackName.ifEmpty { "YourPackName" }
+
+    val codeBlock = """
+        package $packagePlaceholder
+        
+        val $iconPackPlaceholder.MyIcon: ImageVector
+            get() {
+                if (_MyIcon != null) {
+                    return _MyIcon!!
+                }
+            }
+            ...
+    """.trimIndent()
+
+    return codeBlockAnnotatedString(
+        codeBlock = codeBlock,
+        highlightText = packagePlaceholder
+    )
+}
+
+fun buildIconPackHint(iconPackName: String): AnnotatedString {
+    val iconPackPlaceholder = iconPackName.ifEmpty { "YourPackName" }
+    val codeBlock = """
+        object $iconPackPlaceholder
+        
+        val $iconPackPlaceholder.MyIcon: ImageVector
+            get() {
+                if (_MyIcon != null) {
+                    return _MyIcon!!
+                }
+            }
+            ...
+    """.trimIndent()
+
+    return codeBlockAnnotatedString(
+        codeBlock = codeBlock,
+        highlightText = iconPackPlaceholder
+    )
+}
