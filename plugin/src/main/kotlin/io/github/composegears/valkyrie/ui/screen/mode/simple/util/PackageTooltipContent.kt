7815b8bbb8f8c026cfbd51805d616bce13f1006f
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-06-28 17:20:28 +0300

    create CodeBlockAnnotatedString

---
+++ plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/simple/util/PackageTooltipContent.kt
@@ -0,0 +1,24 @@
+package io.github.composegears.valkyrie.ui.screen.mode.simple.util
+
+import androidx.compose.ui.text.AnnotatedString
+import io.github.composegears.valkyrie.ui.util.codeBlockAnnotatedString
+
+fun buildPackageHint(packageName: String): AnnotatedString {
+    val packagePlaceholder = packageName.ifEmpty { "your.package" }
+    val codeBlock = """
+        package $packagePlaceholder
+        
+        val MyIcon: ImageVector
+            get() {
+                if (_MyIcon != null) {
+                return _MyIcon!!
+            }
+            ...
+        }
+       """.trimIndent()
+
+    return codeBlockAnnotatedString(
+        codeBlock = codeBlock,
+        highlightText = packagePlaceholder
+    )
+}
