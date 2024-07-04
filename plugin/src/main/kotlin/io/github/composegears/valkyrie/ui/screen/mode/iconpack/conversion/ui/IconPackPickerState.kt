bb462e116baab44718e4177d93d0d02ddd7d62d6
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-07-04 21:34:40 +0300

    don't handle hover state in icon picker screen

--- plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/iconpack/conversion/ui/IconPackPickerState.kt
+++ plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/iconpack/conversion/ui/IconPackPickerState.kt
@@ -1,13 +1,17 @@
 package io.github.composegears.valkyrie.ui.screen.mode.iconpack.conversion.ui
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -18,12 +22,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.darkrockstudios.libraries.mpfilepicker.DirectoryPicker
 import com.darkrockstudios.libraries.mpfilepicker.MultipleFilePicker
-import io.github.composegears.valkyrie.ui.foundation.DragAndDropBox
 import io.github.composegears.valkyrie.ui.foundation.PreviewWrapper
+import io.github.composegears.valkyrie.ui.foundation.dashedBorder
 import io.github.composegears.valkyrie.ui.foundation.dnd.rememberMultiSelectDragAndDropHandler
 import io.github.composegears.valkyrie.ui.foundation.icons.Collections
 import io.github.composegears.valkyrie.ui.foundation.icons.ValkyrieIcons
@@ -120,6 +126,40 @@ private fun SelectableState(
             }
         }
     }
+}
+
+@Composable
+private fun DragAndDropBox(
+    isDragging: Boolean,
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit
+) {
+    val dashColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    val border by animateDpAsState(if (isDragging) 4.dp else 1.dp)
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth(0.8f)
+            .heightIn(min = 300.dp)
+            .clip(MaterialTheme.shapes.small)
+            .dashedBorder(
+                strokeWidth = border,
+                gapWidth = 8.dp,
+                dashWidth = 8.dp,
+                color = dashColor,
+                shape = MaterialTheme.shapes.small
+            )
+            .padding(2.dp)
+            .background(
+                color = when {
+                    isDragging -> MaterialTheme.colorScheme.primary.copy(alpha = 0.05f)
+                    else -> Color.Transparent
+                },
+                shape = MaterialTheme.shapes.small
+            ),
+        contentAlignment = Alignment.Center,
+        content = content
+    )
 }
 
 @Preview
