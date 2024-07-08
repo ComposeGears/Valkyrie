ab0a61746b7b5e40eb07e8bbb8907d03d03ef582
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-07-08 11:51:16 +0300

    clean up from unused code

--- components/google/src/main/kotlin/androidx/compose/material/icons/generator/Icon.kt
+++ components/google/src/main/kotlin/androidx/compose/material/icons/generator/Icon.kt
@@ -19,16 +19,6 @@ package androidx.compose.material.icons.generator
 /**
  * Represents a icon's Kotlin name, processed XML file name, theme, and XML file content.
  *
- * The [kotlinName] is typically the PascalCase equivalent of the original icon name, with the
- * caveat that icons starting with a number are prefixed with an underscore.
- *
- * @property kotlinName the name of the generated Kotlin property, for example `ZoomOutMap`.
- * @property xmlFileName the name of the processed XML file
- * @property theme the theme of this icon
  * @property fileContent the content of the source XML file that will be parsed.
  */
-data class Icon(
-    val kotlinName: String,
-    val xmlFileName: String,
-    val fileContent: String,
-)
+data class Icon(val fileContent: String)
