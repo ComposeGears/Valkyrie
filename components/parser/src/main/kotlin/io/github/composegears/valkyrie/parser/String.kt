e085c750cda68db6434c49cb3aca90161f079161
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-07-16 13:37:54 +0300

    Update logic for calculation Icon name

---
+++ components/parser/src/main/kotlin/io/github/composegears/valkyrie/parser/String.kt
@@ -0,0 +1,14 @@
+package io.github.composegears.valkyrie.parser
+
+import java.util.*
+
+fun String.removePrefix(prefix: CharSequence): String {
+    if (startsWith(prefix, ignoreCase = true)) {
+        return substring(prefix.length)
+    }
+    return this
+}
+
+fun String.capitalized(): String = replaceFirstChar {
+    if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+}
