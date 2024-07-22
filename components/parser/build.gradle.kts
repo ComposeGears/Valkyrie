15eb90893b1becdebb62afa4423abbc95256076d
Goooler <wangzongler@gmail.com>
2024-07-22 16:48:47 +0800

    Compile only Kotlin stdlib in parser module

--- components/parser/build.gradle.kts
+++ components/parser/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 dependencies {
     api(projects.components.google)
 
+    // https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#kotlin-standard-library
+    compileOnly(libs.kotlin.stdlib)
+
     implementation(libs.android.build.tools)
 
     testImplementation(libs.kotlin.test)
