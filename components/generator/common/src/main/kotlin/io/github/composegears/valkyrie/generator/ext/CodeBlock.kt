632e81bfeacf2620365b5d874458efdd38e63074
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-07-24 16:59:09 +0300

    Fix formatting for gradient path with several params
    Co-authored-by: Zongle Wang <wangzongler@gmail.com>

--- components/generator/common/src/main/kotlin/io/github/composegears/valkyrie/generator/ext/CodeBlock.kt
+++ components/generator/common/src/main/kotlin/io/github/composegears/valkyrie/generator/ext/CodeBlock.kt
@@ -2,8 +2,28 @@ package io.github.composegears.valkyrie.generator.ext
 
 import com.squareup.kotlinpoet.CodeBlock
 
+fun CodeBlock.Builder.argumentBlock(
+    argumentFlow: String,
+    vararg args: Any?,
+    isNested: Boolean = false,
+    block: CodeBlock.Builder.() -> Unit,
+) {
+    add("$argumentFlow\n", *args)
+    indention(block)
+    newLine()
+    add(")")
+    if (isNested) {
+        add(",")
+        newLine()
+    }
+}
+
 fun CodeBlock.Builder.indention(block: CodeBlock.Builder.() -> Unit) {
     indent()
     block()
     unindent()
+}
+
+fun CodeBlock.Builder.newLine() {
+    add("\n")
 }
