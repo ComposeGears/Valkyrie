31d833b92cdf2a114f5237d778d2e8d1bd47bce5
Goooler <wangzongler@gmail.com>
2024-07-28 11:42:33 +0800

    Enable build scan publishing on CI
    Easier to view test reports and track deprecations.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,20 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.develocity") version "3.17.6"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        // TODO: workaround for https://github.com/gradle/gradle/issues/22879.
+        val isCI = providers.environmentVariable("CI").isPresent
+        publishing.onlyIf { isCI }
+    }
+}
+
 dependencyResolutionManagement {
     repositories {
         google {
