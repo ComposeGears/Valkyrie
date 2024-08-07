6019d978dee8dc083c03bf8ffa782eef7739fc3c
Goooler <wangzongler@gmail.com>
2024-08-07 19:50:15 +0800

    Exclude kotlin-compiler-embeddable in distributions

--- components/psi/build.gradle.kts
+++ components/psi/build.gradle.kts
@@ -7,9 +7,10 @@ dependencies {
 
     // https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#kotlin-standard-library
     compileOnly(libs.kotlin.stdlib)
+    // We don't have to include kotlin-compiler-embeddable in distributions.
+    compileOnly(libs.kotlin.embeddable)
 
-    implementation(libs.kotlin.embeddable)
-
+    testImplementation(libs.kotlin.embeddable)
     testImplementation(libs.bundles.test)
     testRuntimeOnly(libs.junit.launcher)
 }
