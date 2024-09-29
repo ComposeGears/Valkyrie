031de379779700387a752dec6c75ebc57e4f119a
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-09-29 17:45:31 +0300

    UI adjustments

--- idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/iconpack/existingpack/ExistingPackScreen.kt
+++ idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/iconpack/existingpack/ExistingPackScreen.kt
@@ -1,8 +1,11 @@
 package io.github.composegears.valkyrie.ui.screen.mode.iconpack.existingpack
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
@@ -10,7 +13,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.composegears.tiamat.koin.koinTiamatViewModel
 import com.composegears.tiamat.navController
 import com.composegears.tiamat.navDestination
@@ -22,6 +24,8 @@ import io.github.composegears.valkyrie.ui.screen.mode.iconpack.existingpack.ui.f
 import io.github.composegears.valkyrie.ui.screen.mode.iconpack.existingpack.ui.model.ExistingPackAction
 import io.github.composegears.valkyrie.ui.screen.mode.iconpack.existingpack.ui.model.ExistingPackEvent
 import io.github.composegears.valkyrie.ui.screen.mode.iconpack.existingpack.ui.model.ExistingPackModeState
+import io.github.composegears.valkyrie.ui.screen.mode.iconpack.existingpack.ui.model.ExistingPackModeState.ChooserState
+import io.github.composegears.valkyrie.ui.screen.mode.iconpack.existingpack.ui.model.ExistingPackModeState.ExistingPackEditState
 import io.github.composegears.valkyrie.ui.screen.mode.iconpack.existingpack.ui.viewmodel.ExistingPackViewModel
 import io.github.composegears.valkyrie.ui.screen.preview.CodePreviewScreen
 import kotlinx.coroutines.Dispatchers
@@ -66,17 +70,26 @@ private fun ExistingPackUi(
     onValueChange: (InputChange) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
+    AnimatedContent(
         modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-        when (state) {
-            is ExistingPackModeState.ChooserState -> {
+        targetState = state,
+        contentKey = {
+            when (it) {
+                is ChooserState -> 0
+                is ExistingPackEditState -> 1
+            }
+        },
+        transitionSpec = {
+            fadeIn(tween(220, delayMillis = 90)) togetherWith fadeOut(tween(90))
+        },
+    ) { current ->
+        when (current) {
+            is ChooserState -> {
                 ChooseExistingPackFile(onAction = onAction)
             }
-            is ExistingPackModeState.ExistingPackEditState -> {
+            is ExistingPackEditState -> {
                 ExistingPackEditor(
-                    state = state,
+                    state = current,
                     onAction = onAction,
                     onValueChange = onValueChange,
                 )
@@ -90,7 +103,7 @@ private fun ExistingPackUi(
 private fun ExistingPackFlowPreview() = PreviewTheme {
     ExistingPackUi(
         modifier = Modifier.fillMaxWidth(0.8f),
-        state = ExistingPackModeState.ChooserState,
+        state = ChooserState,
         onAction = {},
         onValueChange = {},
     )
