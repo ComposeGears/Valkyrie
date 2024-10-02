bf22337c8f14ab81dc14402c5ebf248bf6422d69
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-10-02 16:01:20 +0300

    Remove unnecessary kotlin.LazyThreadSafetyMode import from lazy property export format

--- components/generator/imagevector/src/test/resources/kt/lazy/LinearGradient.kt
+++ components/generator/imagevector/src/test/resources/kt/lazy/LinearGradient.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
-import kotlin.LazyThreadSafetyMode
 
 val LinearGradient: ImageVector by lazy(LazyThreadSafetyMode.NONE) {
     ImageVector.Builder(
--- components/generator/imagevector/src/test/resources/kt/lazy/WithoutPath.pack.preview.kt
+++ components/generator/imagevector/src/test/resources/kt/lazy/WithoutPath.pack.preview.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlin.LazyThreadSafetyMode
 
 val ValkyrieIcons.WithoutPath: ImageVector by lazy(LazyThreadSafetyMode.NONE) {
     ImageVector.Builder(
