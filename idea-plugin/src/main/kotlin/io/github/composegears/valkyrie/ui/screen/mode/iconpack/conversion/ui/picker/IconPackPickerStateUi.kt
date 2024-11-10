bf62bf1f4023e6e3a61d68ece76379712fd288b4
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-11-10 16:41:17 +0300

    [IconPack] Allow to paste icon from clipboard

--- idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/iconpack/conversion/ui/picker/IconPackPickerStateUi.kt
+++ idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/iconpack/conversion/ui/picker/IconPackPickerStateUi.kt
@@ -3,6 +3,7 @@ package io.github.composegears.valkyrie.ui.screen.mode.iconpack.conversion.ui.pi
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -14,26 +15,47 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import io.github.composegears.valkyrie.ui.foundation.AppBarTitle
+import io.github.composegears.valkyrie.ui.foundation.BackAction
+import io.github.composegears.valkyrie.ui.foundation.CenterVerticalRow
+import io.github.composegears.valkyrie.ui.foundation.SettingsAction
+import io.github.composegears.valkyrie.ui.foundation.TopAppBar
+import io.github.composegears.valkyrie.ui.foundation.VerticalSpacer
 import io.github.composegears.valkyrie.ui.foundation.WeightSpacer
 import io.github.composegears.valkyrie.ui.foundation.dashedBorder
+import io.github.composegears.valkyrie.ui.foundation.disabled
 import io.github.composegears.valkyrie.ui.foundation.icons.AddFile
 import io.github.composegears.valkyrie.ui.foundation.icons.ValkyrieIcons
+import io.github.composegears.valkyrie.ui.foundation.onPasteEvent
 import io.github.composegears.valkyrie.ui.foundation.rememberMutableState
 import io.github.composegears.valkyrie.ui.foundation.theme.PreviewTheme
+import io.github.composegears.valkyrie.ui.platform.ClipboardDataType
+import io.github.composegears.valkyrie.ui.platform.Os
+import io.github.composegears.valkyrie.ui.platform.pasteFromClipboard
 import io.github.composegears.valkyrie.ui.platform.picker.rememberDirectoryPicker
 import io.github.composegears.valkyrie.ui.platform.picker.rememberMultipleFilesPicker
+import io.github.composegears.valkyrie.ui.platform.rememberCurrentOs
 import io.github.composegears.valkyrie.ui.platform.rememberMultiSelectDragAndDropHandler
 import io.github.composegears.valkyrie.ui.screen.mode.iconpack.conversion.PickerEvent
 import io.github.composegears.valkyrie.ui.screen.mode.iconpack.conversion.PickerEvent.PickDirectory
@@ -43,20 +65,52 @@ import kotlin.io.path.isDirectory
 import kotlin.io.path.isRegularFile
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun IconPackPickerStateUi(
-    modifier: Modifier = Modifier,
     onPickerEvent: (PickerEvent) -> Unit,
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    openSettings: () -> Unit,
 ) {
     val scope = rememberCoroutineScope()
 
     val multipleFilePicker = rememberMultipleFilesPicker()
     val directoryPicker = rememberDirectoryPicker()
 
+    val focusRequester = remember { FocusRequester() }
+
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .focusRequester(focusRequester)
+            .focusProperties { exit = { focusRequester } }
+            .focusable()
+            .onPointerEvent(PointerEventType.Enter) {
+                focusRequester.requestFocus()
+            }
+            .onPointerEvent(PointerEventType.Exit) {
+                focusRequester.freeFocus()
+            }
+            .onPasteEvent {
+                when (val clipboardData = pasteFromClipboard()) {
+                    is ClipboardDataType.Files -> {
+                        onPickerEvent(PickFiles(paths = clipboardData.paths))
+                    }
+                    is ClipboardDataType.Text -> {
+                        onPickerEvent(PickerEvent.ClipboardText(clipboardData.text))
+                    }
+                    null -> {}
+                }
+            },
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        TopAppBar {
+            BackAction(onBack = onBack)
+            AppBarTitle(title = "IconPack generation")
+            WeightSpacer()
+            SettingsAction(openSettings = openSettings)
+        }
         WeightSpacer(weight = 0.3f)
         SelectableState(
             onSelectPath = { paths ->
@@ -93,6 +147,10 @@ fun IconPackPickerStateUi(
         )
         WeightSpacer(weight = 0.7f)
     }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 }
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -105,17 +163,35 @@ private fun SelectableState(
     val dragAndDropHandler = rememberMultiSelectDragAndDropHandler(onDrop = onSelectPath)
     val isDragging by rememberMutableState(dragAndDropHandler.isDragging) { dragAndDropHandler.isDragging }
 
+    val os = rememberCurrentOs()
+
     DragAndDropBox(isDragging = isDragging) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Icon(
-                imageVector = ValkyrieIcons.AddFile,
-                contentDescription = null,
-            )
+            CenterVerticalRow {
+                Icon(
+                    imageVector = ValkyrieIcons.AddFile,
+                    contentDescription = null,
+                )
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = "Drag & drop",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleSmall,
+                )
+            }
             Text(
-                modifier = Modifier.padding(8.dp),
-                text = "Drag & drop\n\nor",
+                text = when (os) {
+                    Os.MacOS -> "Cmd+V to paste from clipboard"
+                    else -> "Ctrl+V to paste from clipboard"
+                },
                 textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleSmall,
+                color = LocalContentColor.current.disabled(),
+                style = MaterialTheme.typography.labelSmall,
+            )
+            VerticalSpacer(16.dp)
+            Text(
+                text = "or",
+                style = MaterialTheme.typography.labelMedium,
             )
             FlowRow(
                 modifier = Modifier.fillMaxWidth(),
@@ -169,5 +245,9 @@ private fun DragAndDropBox(
 @Preview
 @Composable
 private fun PickerStatePreview() = PreviewTheme {
-    IconPackPickerStateUi(onPickerEvent = {})
+    IconPackPickerStateUi(
+        onPickerEvent = {},
+        onBack = {},
+        openSettings = {},
+    )
 }
