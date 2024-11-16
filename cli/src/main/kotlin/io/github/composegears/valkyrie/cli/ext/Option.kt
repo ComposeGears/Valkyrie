4d494cec5035fd5c6b5260c72bc3dcf7dfaa1692
Yahor Urbanovich <yahor_urbanovich@epam.com>
2024-11-16 14:51:01 +0300

    Update SvgXmlToImageVectorCommand

--- cli/src/main/kotlin/io/github/composegears/valkyrie/cli/ext/Option.kt
+++ cli/src/main/kotlin/io/github/composegears/valkyrie/cli/ext/Option.kt
@@ -1,12 +1,22 @@
 package io.github.composegears.valkyrie.cli.ext
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.boolean
 import com.github.ajalt.clikt.parameters.types.int
-import io.github.composegears.valkyrie.generator.imagevector.OutputFormat
+import com.github.ajalt.clikt.parameters.types.path
+
+internal fun CliktCommand.requiredStringOption(
+    vararg names: String,
+    help: String,
+) = option(names = names, help = help).required()
+
+internal fun CliktCommand.requiredPathOption(
+    vararg names: String,
+    help: String,
+) = option(names = names, help = help).path().required()
 
 internal fun CliktCommand.stringOption(
     vararg names: String,
@@ -18,21 +28,10 @@ internal fun CliktCommand.booleanOption(
     vararg names: String,
     help: String,
     default: Boolean = false,
-) = option(names = names, help = help).flag(default = default)
+) = option(names = names, help = help).boolean().default(default)
 
 internal fun CliktCommand.intOption(
     vararg names: String,
     help: String,
     default: Int,
 ) = option(names = names, help = help).int().default(default)
-
-internal fun CliktCommand.outputFormatOption() = option(
-    "--output-format",
-    help = "ImageVector output format, it must be backing-property or lazy-property.",
-).convert {
-    when (it.lowercase()) {
-        "backing-property" -> OutputFormat.BackingProperty
-        "lazy-property" -> OutputFormat.LazyProperty
-        else -> error("Invalid output format, it must be backing-property or lazy-property.")
-    }
-}.default(OutputFormat.BackingProperty)
--- cli/src/main/proguard-rules.pro
+++ cli/src/main/proguard-rules.pro
@@ -6,10 +6,15 @@
   public static void main(java.lang.String[]);
 }
 
+-keepclassmembers enum * {
+    public static **[] values();
+    public static ** valueOf(java.lang.String);
+}
+
+-keep class org.xmlpull.v1.** { *; }
+-keep class org.xmlpull.mxp1.** { *; }
+
 -dontwarn com.google.j2objc.annotations.**
 -dontwarn org.graalvm.nativeimage.**
 -dontwarn org.graalvm.word.**
 -dontwarn com.oracle.svm.core.annotate.**
-
-# TODO: it should be possible to keep less classes under this package.
--keep class org.xmlpull.**
