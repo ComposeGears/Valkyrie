86387f723fa0679e2912f31f149d44bda67cd55e
Yahor Urbanovich <yahor_urbanovich@epam.com>
2025-03-10 17:47:43 +0300

    [CLI] Migrate to hierarchical icon pack structure

--- components/generator/common/src/main/kotlin/io/github/composegears/valkyrie/generator/model/IconPack.kt
+++ components/generator/common/src/main/kotlin/io/github/composegears/valkyrie/generator/model/IconPack.kt
@@ -3,4 +3,31 @@ package io.github.composegears.valkyrie.generator.model
 data class IconPack(
     val name: String,
     val nested: List<IconPack> = emptyList(),
-)
+) {
+
+    override fun toString(): String = buildString {
+        appendLine()
+        append(name)
+        if (name.isNotEmpty()) {
+            append(":")
+        }
+        buildNestedTree(prefix = "", items = nested)
+    }
+
+    private fun StringBuilder.buildNestedTree(prefix: String, items: List<IconPack>) {
+        items.forEachIndexed { index, iconPack ->
+            val isLastChild = index == items.lastIndex
+            val branchChar = if (isLastChild) "└── " else "├── "
+            val nextPrefix = prefix + if (isLastChild) "\t" else "│\t"
+
+            appendLine()
+            append(prefix)
+            append(branchChar)
+            append(iconPack.name)
+
+            if (iconPack.nested.isNotEmpty()) {
+                buildNestedTree(prefix = nextPrefix, items = iconPack.nested)
+            }
+        }
+    }
+}
