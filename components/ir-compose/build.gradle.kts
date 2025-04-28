b5124dd55bdbd79cd73e83d6cec2c488912b78f0
Yahor Urbanovich <yahor_urbanovich@epam.com>
2025-04-28 16:13:54 +0300

    Migrate ir-compose to kmp

--- components/ir-compose/build.gradle.kts
+++ components/ir-compose/build.gradle.kts
@@ -1,12 +1,26 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.compose)
 }
 
-dependencies {
-    api(projects.components.ir)
+kotlin {
+    jvm()
 
-    implementation(compose.material3)
-    implementation(compose.ui)
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            api(projects.components.ir)
+
+            implementation(compose.material3)
+            implementation(compose.ui)
+        }
+    }
 }
