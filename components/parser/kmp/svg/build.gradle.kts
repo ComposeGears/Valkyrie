ea1ac002da426416a02f94e291cbf09a05ba5f4b
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2025-05-02 20:27:04 +1000

    Add common module to kmp/svg module

--- components/parser/kmp/svg/build.gradle.kts
+++ components/parser/kmp/svg/build.gradle.kts
@@ -7,6 +7,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(projects.components.ir)
+            implementation(projects.components.parser.common)
             implementation(libs.xmlutil)
         }
         commonTest.dependencies {
