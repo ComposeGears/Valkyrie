5bcbfd83c60ae432201c3da6038392deee8e809f
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2025-05-18 13:29:36 +1000

    Remove duplicate code and reuse existing functions
    Extract IconParserOutput
    
    Add requirement checks and cover with tests

--- components/parser/jvm/svg/src/main/kotlin/io/github/composegears/valkyrie/parser/jvm/svg/SvgToXmlParser.kt
+++ components/parser/jvm/svg/src/main/kotlin/io/github/composegears/valkyrie/parser/jvm/svg/SvgToXmlParser.kt
@@ -1,29 +1,17 @@
 package io.github.composegears.valkyrie.parser.jvm.svg
 
 import com.android.ide.common.vectordrawable.Svg2Vector
+import java.io.ByteArrayOutputStream
 import java.nio.file.Path
 import kotlin.io.path.createTempFile
-import kotlin.io.path.outputStream
-import kotlin.io.path.readText
 import kotlin.io.path.writeText
 
 object SvgToXmlParser {
 
-    fun parse(path: Path): String {
-        val tmpPath = createTempFile()
-        parse(path, tmpPath)
-        return tmpPath.readText()
-    }
+    fun parse(input: String): String = parse(createTempFile().apply { writeText(input) })
 
-    fun parse(input: String): String {
-        val tmpInPath = createTempFile().apply { writeText(input) }
-        val tmpOutPath = createTempFile()
-
-        parse(tmpInPath, tmpOutPath)
-        return tmpOutPath.readText()
-    }
-
-    private fun parse(input: Path, output: Path) {
-        Svg2Vector.parseSvgToXml(input, output.outputStream())
+    fun parse(input: Path): String = ByteArrayOutputStream().use {
+        Svg2Vector.parseSvgToXml(input, it)
+        it.toString()
     }
 }
