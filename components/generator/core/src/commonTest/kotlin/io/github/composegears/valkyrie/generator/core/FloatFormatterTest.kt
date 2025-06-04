f96211bdd7d5d5c330a68b588d7b9f658a1bb4fb
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2025-06-04 23:11:46 +1000

    Update Kotlin version to 2.2.0-RC2 and adjust wasm FloatFormatterTest

--- components/generator/core/src/commonTest/kotlin/io/github/composegears/valkyrie/generator/core/FloatFormatterTest.kt
+++ components/generator/core/src/commonTest/kotlin/io/github/composegears/valkyrie/generator/core/FloatFormatterTest.kt
@@ -2,7 +2,6 @@ package io.github.composegears.valkyrie.generator.core
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import assertk.assertions.startsWith
 import kotlin.test.Test
 
 class FloatFormatterTest {
@@ -15,7 +14,6 @@ class FloatFormatterTest {
 
         assertThat(24f.formatFloat()).isEqualTo("24f")
         assertThat(24.0f.formatFloat()).isEqualTo("24f")
-        // NOTE: issue of floating-point precision. wasmJS will result in 24.000019...
-        assertThat(24.00002f.formatFloat()).startsWith("24.0000")
+        assertThat(24.00002f.formatFloat()).isEqualTo("24.00002f")
     }
 }
