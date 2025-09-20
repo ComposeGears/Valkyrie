74a6548047a47df3c36a9e1ac5f37431c72079ec
t-regbs <aregbestimi@gmail.com>
2025-09-20 23:17:18 +0100

    Add ImageVector to xml parser

--- components/parser/common/src/commonMain/kotlin/io/github/composegears/valkyrie/parser/common/PathNode.kt
+++ components/parser/common/src/commonMain/kotlin/io/github/composegears/valkyrie/parser/common/PathNode.kt
@@ -166,6 +166,90 @@ private inline fun pathNodesFromArgs(
     }
 }
 
+fun IrPathNode.toPathCommand(): Char = when (this) {
+    is Close -> 'Z'
+    is MoveTo -> 'M'
+    is RelativeMoveTo -> 'm'
+    is LineTo -> 'L'
+    is RelativeLineTo -> 'l'
+    is HorizontalTo -> 'H'
+    is RelativeHorizontalTo -> 'h'
+    is VerticalTo -> 'V'
+    is RelativeVerticalTo -> 'v'
+    is CurveTo -> 'C'
+    is RelativeCurveTo -> 'c'
+    is ReflectiveCurveTo -> 'S'
+    is RelativeReflectiveCurveTo -> 's'
+    is QuadTo -> 'Q'
+    is RelativeQuadTo -> 'q'
+    is ReflectiveQuadTo -> 'T'
+    is RelativeReflectiveQuadTo -> 't'
+    is ArcTo -> 'A'
+    is RelativeArcTo -> 'a'
+}
+
+/**
+ * Extension function to convert an IrPathNode back to its argument array.
+ */
+fun IrPathNode.toPathArgs(): FloatArray = when (this) {
+    is Close -> floatArrayOf()
+    is MoveTo -> floatArrayOf(x, y)
+    is RelativeMoveTo -> floatArrayOf(x, y)
+    is LineTo -> floatArrayOf(x, y)
+    is RelativeLineTo -> floatArrayOf(x, y)
+    is HorizontalTo -> floatArrayOf(x)
+    is RelativeHorizontalTo -> floatArrayOf(x)
+    is VerticalTo -> floatArrayOf(y)
+    is RelativeVerticalTo -> floatArrayOf(y)
+    is CurveTo -> floatArrayOf(x1, y1, x2, y2, x3, y3)
+    is RelativeCurveTo -> floatArrayOf(dx1, dy1, dx2, dy2, dx3, dy3)
+    is ReflectiveCurveTo -> floatArrayOf(x1, y1, x2, y2)
+    is RelativeReflectiveCurveTo -> floatArrayOf(x1, y1, x2, y2)
+    is QuadTo -> floatArrayOf(x1, y1, x2, y2)
+    is RelativeQuadTo -> floatArrayOf(x1, y1, x2, y2)
+    is ReflectiveQuadTo -> floatArrayOf(x, y)
+    is RelativeReflectiveQuadTo -> floatArrayOf(x, y)
+    is ArcTo -> floatArrayOf(
+        horizontalEllipseRadius, verticalEllipseRadius, theta,
+        if (isMoreThanHalf) 1f else 0f,
+        if (isPositiveArc) 1f else 0f,
+        arcStartX, arcStartY,
+    )
+    is RelativeArcTo -> floatArrayOf(
+        horizontalEllipseRadius, verticalEllipseRadius, theta,
+        if (isMoreThanHalf) 1f else 0f,
+        if (isPositiveArc) 1f else 0f,
+        arcStartDx, arcStartDy,
+    )
+}
+
+/**
+ * Extension function to convert an IrPathNode back to its path string representation.
+ */
+fun IrPathNode.toPathString(formatFloat: (Float) -> String = { it.toString() }): String {
+    val command = toPathCommand()
+    val args = toPathArgs()
+
+    return if (args.isEmpty()) {
+        command.toString()
+    } else {
+        buildString {
+            append(command)
+            args.forEach { arg ->
+                append(' ')
+                append(formatFloat(arg))
+            }
+        }
+    }
+}
+
+/**
+ * Extension function to convert a list of IrPathNodes to a complete path data string.
+ */
+fun List<IrPathNode>.toPathString(formatFloat: (Float) -> String = { it.toString() }): String {
+    return joinToString(" ") { it.toPathString(formatFloat) }
+}
+
 /**
  * Constants for the number of expected arguments for a given node. If the number of received
  * arguments is a multiple of these, the excess will be converted into additional path nodes.
