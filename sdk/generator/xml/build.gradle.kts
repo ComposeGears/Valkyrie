a8234611b4a61d975602e95f86c759759ee919cc
Yahor Urbanovich <egorikftp@gmail.com>
2025-09-25 22:52:38 +0300

    introduce "sdk:generator:xml" for xml generation logic

---
+++ sdk/generator/xml/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.valkyrie.kmp)
+    alias(libs.plugins.valkyrie.kover)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.sdk.core.xml)
+            implementation(projects.components.ir)
+        }
+    }
+}
+
+tasks.withType<Jar>().configureEach {
+    archiveBaseName = "sdk-generator-xml"
+}
