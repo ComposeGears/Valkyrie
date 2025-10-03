3618da13819f9c932f26122c21686debf8df29b8
t-regbs <aregbestimi@gmail.com>
2025-10-03 19:22:23 +0100

    remove unnecessary files after updating branch

--- components/parser/unified/src/jvmMain/kotlin/io/github/composegears/valkyrie/parser/unified/IrToVectorXml.jvm.kt
+++
@@ -1,6 +0,0 @@
-package io.github.composegears.valkyrie.parser.unified
-
-import io.github.composegears.valkyrie.ir.IrImageVector
-import io.github.composegears.valkyrie.parser.kmp.xml.ImageVectorToXmlParser
-
-actual fun IrImageVector.toVectorXmlString(): String = ImageVectorToXmlParser.parse(this)
