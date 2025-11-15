c8e4ef9bad2aea2731b7f742fe3f16e5aa0f8d2d
Yahor Urbanovich <egorikftp@gmail.com>
2025-11-15 21:40:04 +0300

    Rename buildconfig plugin

--- tools/cli/build.gradle.kts
+++ tools/cli/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.buildConfig)
+    alias(libs.plugins.buildconfig)
     alias(libs.plugins.shadow)
     alias(libs.plugins.valkyrie.kover)
     application
