466c90cf1ed96d59ac62e9b38f69e8f68dc315aa
Yahor Urbanovich <egorikftp@gmail.com>
2025-11-30 15:37:33 +0300

    [Gradle] Create CommonGradleTest, add simple conversion example in README

---
+++ tools/gradle-plugin/src/test/kotlin/io/github/composegears/valkyrie/gradle/NoPackPluginConfigurationTest.kt
@@ -0,0 +1,65 @@
+package io.github.composegears.valkyrie.gradle
+
+import assertk.assertThat
+import assertk.assertions.contains
+import assertk.assertions.containsMatch
+import assertk.assertions.exists
+import assertk.assertions.isEqualTo
+import io.github.composegears.valkyrie.gradle.common.GENERATED_SOURCES_DIR
+import io.github.composegears.valkyrie.gradle.internal.TASK_NAME
+import java.nio.file.Path
+import kotlin.io.path.readText
+import kotlin.io.path.walk
+import kotlin.io.path.writeText
+import org.gradle.testkit.runner.TaskOutcome.SKIPPED
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+
+class NoPackPluginConfigurationTest : CommonGradleTest() {
+
+    @Test
+    fun `no pack generation configuration from commonMain sourceSet`(@TempDir root: Path) {
+        root.writeSettingsFile()
+
+        root.resolve("build.gradle.kts").writeText(
+            """
+                plugins {
+                    kotlin("multiplatform")
+                    id("io.github.composegears.valkyrie")
+                }
+                valkyrie {
+                    packageName = "x.y.z"
+                }
+                kotlin {
+                    jvm()
+                }
+            """.trimIndent(),
+        )
+
+        root.writeTestDrawables(sourceSet = "commonMain")
+
+        val result = runTask(root, TASK_NAME)
+
+        assertThat(result).taskHadResult(":generateValkyrieImageVectorCommonTest", SKIPPED)
+        assertThat(result).taskHadResult(":generateValkyrieImageVectorJvmMain", SKIPPED)
+        assertThat(result).taskHadResult(":generateValkyrieImageVectorJvmTest", SKIPPED)
+        assertThat(result).taskWasSuccessful(":$TASK_NAME")
+        assertThat(result.output).contains("Generated 17 ImageVectors in package x.y.z")
+
+        val generatedIcons = root.resolve(GENERATED_SOURCES_DIR).walk().toList()
+        assertThat(generatedIcons.count()).isEqualTo(17)
+
+        generatedIcons.forEach { file ->
+            assertThat(file.readText()).containsMatch(Regex("val [A-Za-z0-9_]+: ImageVector"))
+        }
+
+        listOf(
+            "OnlyPath.kt",
+            "IconWithShorthandColor.kt",
+            "SeveralPath.kt",
+            "AllPathParams.kt",
+        ).forEach { filename ->
+            assertThat(root.resolve("${GENERATED_SOURCES_DIR}/commonMain/x/y/z/$filename")).exists()
+        }
+    }
+}
---
+++ tools/gradle-plugin/src/test/kotlin/io/github/composegears/valkyrie/gradle/common/TestConstant.kt
@@ -0,0 +1,5 @@
+package io.github.composegears.valkyrie.gradle.common
+
+import io.github.composegears.valkyrie.gradle.internal.DEFAULT_GENERATED_SOURCES_DIR
+
+internal const val GENERATED_SOURCES_DIR = "build/$DEFAULT_GENERATED_SOURCES_DIR"
