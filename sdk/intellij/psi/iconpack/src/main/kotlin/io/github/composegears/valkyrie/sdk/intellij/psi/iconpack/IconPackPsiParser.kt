c430dbec78cd0e8333a694fb949371b11a514b1d
Yahor Urbanovich <egorikftp@gmail.com>
2025-12-14 15:30:53 +0300

    Move :psi:iconpack into :sdk:intellij:psi:iconpack

--- sdk/intellij/psi/iconpack/src/main/kotlin/io/github/composegears/valkyrie/sdk/intellij/psi/iconpack/IconPackPsiParser.kt
+++ sdk/intellij/psi/iconpack/src/main/kotlin/io/github/composegears/valkyrie/sdk/intellij/psi/iconpack/IconPackPsiParser.kt
@@ -1,4 +1,4 @@
-package io.github.composegears.valkyrie.psi.iconpack
+package io.github.composegears.valkyrie.sdk.intellij.psi.iconpack
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiManager
