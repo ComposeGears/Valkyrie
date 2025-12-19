5e383f59207c8f5722de90628401ae467aeb8aa0
Yahor Urbanovich <egorikftp@gmail.com>
2025-12-19 12:07:32 +0300

    Enhance XmlCodeViewer with customizable text field styles

--- tools/idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/imagevectortoxml/conversion/ui/XmlCodeViewer.kt
+++ tools/idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/mode/imagevectortoxml/conversion/ui/XmlCodeViewer.kt
@@ -1,7 +1,11 @@
 package io.github.composegears.valkyrie.ui.screen.mode.imagevectortoxml.conversion.ui
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import io.github.composegears.valkyrie.compose.codeviewer.core.CodeEditor
 import io.github.composegears.valkyrie.compose.codeviewer.core.rememberCodeHighlight
 
@@ -17,5 +21,14 @@ fun XmlCodeViewer(
         modifier = modifier,
         highlights = highlights,
         onValueChange = onChange,
+        textStyle = MaterialTheme.typography.bodyMedium.copy(lineHeight = 21.sp, fontSize = 13.sp),
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent,
+        ),
     )
 }
