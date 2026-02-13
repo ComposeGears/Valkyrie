c7394ab669a623c9d2846e95db9264029d4af387
t-regbs <aregbestimi@gmail.com>
2026-02-13 01:22:53 +0000

    test: add tests for invalid android color inputs to AndroidColorParserTest

--- components/parser/common/src/commonTest/kotlin/io/github/composegears/valkyrie/parser/common/AndroidColorParserTest.kt
+++ components/parser/common/src/commonTest/kotlin/io/github/composegears/valkyrie/parser/common/AndroidColorParserTest.kt
@@ -2,6 +2,7 @@ package io.github.composegears.valkyrie.parser.common
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isNull
 import io.github.composegears.valkyrie.sdk.ir.core.IrColor
 import kotlin.test.Test
 
@@ -15,6 +16,16 @@ class AndroidColorParserTest {
             val androidColorName = "@android:color/$colorName"
 
             assertThat(actual = AndroidColorParser.parse(androidColorName)).isEqualTo(IrColor(hexValue))
+        }
+    }
+
+    @Test
+    fun `test parsing invalid android colors returns null`() {
+        with(AndroidColorParser) {
+            assertThat(parse("@android:color/non_existent_color")).isNull()
+            assertThat(parse("@android:color/")).isNull()
+            assertThat(parse("invalid_prefix/red")).isNull()
+            assertThat(parse("")).isNull()
         }
     }
 }
