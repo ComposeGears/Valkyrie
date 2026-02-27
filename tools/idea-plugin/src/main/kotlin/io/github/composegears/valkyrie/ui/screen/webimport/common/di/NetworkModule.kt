56b689b95b075476d1b4fe1e918fb67e19c6bcfd
Yahor Urbanovich <egorikftp@gmail.com>
2026-02-27 22:24:46 +0300

    [Web Import] Enable persistent cache to avoid re-fetching icons on every plugin restart

--- tools/idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/webimport/common/di/NetworkModule.kt
+++ tools/idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/webimport/common/di/NetworkModule.kt
@@ -1,11 +1,16 @@
 package io.github.composegears.valkyrie.ui.screen.webimport.common.di
 
 import com.composegears.leviathan.Leviathan
+import com.intellij.openapi.application.PathManager
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.cache.storage.FileStorage
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
+import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.time.Duration.Companion.seconds
 import kotlinx.serialization.json.Json
 
@@ -25,6 +30,11 @@ object NetworkModule : Leviathan() {
             }
             install(ContentNegotiation) {
                 json(inject(json))
+            }
+            install(HttpCache) {
+                val cacheDir = Paths.get(PathManager.getSystemPath()).resolve("valkyrie")
+                val cacheFile = Files.createDirectories(cacheDir).toFile()
+                publicStorage(FileStorage(cacheFile))
             }
         }
     }
