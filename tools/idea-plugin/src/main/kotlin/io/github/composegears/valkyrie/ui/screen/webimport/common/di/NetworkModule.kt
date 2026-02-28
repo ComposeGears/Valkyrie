f2f61341eabda8e08b11c7ff08c27222601650cc
Yahor Urbanovich <egorikftp@gmail.com>
2026-02-28 14:02:18 +0300

    Add runCatching for cache initialization

--- tools/idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/webimport/common/di/NetworkModule.kt
+++ tools/idea-plugin/src/main/kotlin/io/github/composegears/valkyrie/ui/screen/webimport/common/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package io.github.composegears.valkyrie.ui.screen.webimport.common.di
 
 import com.composegears.leviathan.Leviathan
 import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.diagnostic.Logger
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.HttpTimeout
@@ -32,9 +33,17 @@ object NetworkModule : Leviathan() {
                 json(inject(json))
             }
             install(HttpCache) {
-                val cacheDir = Paths.get(PathManager.getSystemPath()).resolve("valkyrie")
-                val cacheFile = Files.createDirectories(cacheDir).toFile()
-                publicStorage(FileStorage(cacheFile))
+                runCatching {
+                    val cacheDir = Paths.get(PathManager.getSystemPath())
+                        .resolve("valkyrie")
+                        .resolve("http-cache")
+                    val cacheFile = Files.createDirectories(cacheDir).toFile()
+
+                    publicStorage(FileStorage(cacheFile))
+                }.onFailure {
+                    Logger.getInstance(NetworkModule::class.java)
+                        .error("Failed to create http cache dir: ${it.message}")
+                }
             }
         }
     }
