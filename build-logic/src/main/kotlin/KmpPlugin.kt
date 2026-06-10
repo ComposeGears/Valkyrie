316a00a122f9af701c6e078f7d265c97f75c4ff9
Yahor Urbanovich <egorikftp@gmail.com>
2026-06-10 18:03:23 +0300

    Add ExplicitApiPlugin for enhanced API visibility

--- build-logic/src/main/kotlin/KmpPlugin.kt
+++ build-logic/src/main/kotlin/KmpPlugin.kt
@@ -1,6 +1,6 @@
 import io.github.composegears.valkyrie.extension.applyTargets
 import io.github.composegears.valkyrie.internal.configureArchiveBaseName
-import io.github.composegears.valkyrie.internal.kmpExtension
+import io.github.composegears.valkyrie.internal.kotlinMultiplatform
 import io.github.composegears.valkyrie.internal.kotlinMultiplatformPluginId
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -12,7 +12,7 @@ class KmpPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         apply(plugin = kotlinMultiplatformPluginId)
 
-        kmpExtension {
+        kotlinMultiplatform {
             applyTargets()
 
             compilerOptions {
